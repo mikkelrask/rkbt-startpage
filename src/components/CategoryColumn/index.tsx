7eb1f93edd6521b1922a5730cf450853541098f6
Mikkel Rask <mikkelrask@protonmail.com>
2023-07-25 20:09:15 +0200

    added Link component

--- src/components/CategoryColumn/index.tsx
+++ src/components/CategoryColumn/index.tsx
@@ -1,13 +1,17 @@
 // Categories.tsx
 import React, { useEffect, useState } from "react";
+import Link from "../Link";
 
-const endpoint = "/api/categories";
+const categories = "/api/categories";
+const links = "/api/links";
 const apiBaseUrl = "http://localhost:3001";
-const apiUrl = `${apiBaseUrl}${endpoint}`;
+const apiUrl = `${apiBaseUrl}${categories}`;
 
 interface Category {
   id: number;
   name: string;
+  icon: string; // Add the 'icon' property for storing the Font Awesome icon name
+  links: Array<{ id: number; title: string; url: string }>; // Assuming the link structure
 }
 
 const Categories: React.FC = () => {
@@ -38,13 +42,26 @@ const Categories: React.FC = () => {
 
   return (
     <div>
-      <ul>
-        {categories.map((category) => (
-          <li key={category.id}>{category.name}</li>
-        ))}
-      </ul>
+      {categories.map((category) => (
+        <div key={category.id} className="container">
+          <h2 className="title">
+            <i className={category.icon}></i>
+            {category.name}
+          </h2>
+          <ul>
+            {category.links?.map(
+              (
+                link // Add the nullish coalescing operator here
+              ) => (
+                <li class="title" key={link.id}>
+                  <Link url={link.url} title={link.title} />
+                </li>
+              )
+            )}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
-
 export default Categories;
---
+++ src/components/Link/index.tsx
@@ -0,0 +1,17 @@
+// Link.tsx
+import React from "react";
+
+interface LinkProps {
+  url: string;
+  title: string;
+}
+
+const Link: React.FC<LinkProps> = ({ url, title }) => {
+  return (
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      {title}
+    </a>
+  );
+};
+
+export default Link;
