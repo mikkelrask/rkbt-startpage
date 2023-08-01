5b7a4c2b88ae4350ac720bb27ff05d5a7daedf0b
Mikkel Rask <mikkelrask@protonmail.com>
2023-08-01 13:55:32 +0200

    upload it all

--- src/skotland/skotland.tsx
+++ src/skotland/skotland.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AddLink from "../components/addLink
-import "./index.css";
+import AddLink from "../components/addLink/index.tsx";
+import "../index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
