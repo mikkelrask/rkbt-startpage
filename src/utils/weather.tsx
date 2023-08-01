0ac206ba7f5be4e8d4bf88813618941f8558273f
Mikkel Rask <mikkelrask@protonmail.com>
2023-08-01 13:35:09 +0200

    weather is now acomponent

--- src/utils/weather.tsx
+++ src/utils/weather.tsx
@@ -1,21 +1,20 @@
-import config from './config/config.js'
-const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${config.LAT}&lon=${config.LON}&appid=${config.TOKEN}`
+import config from './config/config.js';
+
+const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${config.LAT}&lon=${config.LON}&appid=${config.TOKEN}`;
+
+console.log(URL, config.TOKEN, config.LAT, config.LON);
 
 const GetWeatherData = async () => {
-    
-async function name(params:type) {
-    try {
-      const response = await fetch(URL);
-      const data = await response.json();
-      console.log(data.weather[0].main);
-      console.log(response);
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      return (data.weather[0].main);
-    } catch (e) {
-      console.error('Failed to fetch weather data. See response', e);
-      return null;
-      }
-}
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data.weather[0].main);
+    console.log(response);
+    return data.weather[0].description;
+  } catch (e) {
+    console.error('Failed to fetch weather data. See response', e);
+    return null;
   }
- 
-GetWeatherData();
+};
+
+export default GetWeatherData;
