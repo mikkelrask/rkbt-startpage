20b3c4c8ca178dc5c49da7fb1374329ccabc8a86
Mikkel Rask <mikkelrask@protonmail.com>
2023-08-01 13:49:37 +0200

    weather is added to the header

--- src/components/HeaderContainer/index.tsx
+++ src/components/HeaderContainer/index.tsx
@@ -1,21 +1,32 @@
-import { T } from '@tolgee/react'
+import { useEffect, useState } from 'react';
+import { T } from '@tolgee/react';
+import GetWeatherData from '../../utils/weather'; // Remove the 'default' keyword
 
 const HeaderContainer = () => {
-    return (
-        <>
-            <div className="header-container">
-                <h1><T keyName="welcome" /></h1>
-                <div className="status-strip">
-                    <span className="date"></span>
-                    <span> | </span>
-                    <span className="time"></span>
-                    <span> | </span>
-                    <span className="weather">
-                    </span>
-                </div>
-            </div>
-        </>
-    );
-}
+  const [weatherData, setWeatherData] = useState('');
+
+  useEffect(() => {
+    GetWeatherData().then((data) => {
+      setWeatherData(data);
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="header-container">
+        <h1><T keyName="Velkommen hjem" /></h1>
+        <div className="status-strip">
+          <span className="date"></span>
+          <span> | </span>
+          <span className="time"></span>
+          <span> | </span>
+          <span className="weather">
+            {weatherData}
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default HeaderContainer;
