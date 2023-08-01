84d1caac8447b793eb14d22e3244115a1b47ff3e
Mikkel Rask <mikkelrask@protonmail.com>
2023-08-01 14:41:45 +0200

    remove comment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import react from "@vitejs/plugin-react-swc";
 const root = resolve(__dirname, "src");
 const outDir = resolve(__dirname, "dist");
 
-// https://vitejs.dev/config/
 export default defineConfig({
   root,
   plugins: [react()],
