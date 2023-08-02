15acd2cd1158c0117c0ad4310a98855dcb97ad77
Mikkel Rask <mikkelrask@protonmail.com>
2023-08-02 15:59:49 +0200

    maybe

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ import "./App.css";
 function App() {
   return (
     <>
-
-        <BigImage />
-
+      <BigImage />
       <HeaderContainer />
       <div className="card-container">
         <Categories />
--- src/main.tsx
+++ src/main.tsx
@@ -2,29 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Tolgee, DevTools, TolgeeProvider, FormatSimple } from "@tolgee/react";
-
-const envApiURL = import.meta.env.VITE_REACT_APP_TOLGEE_API_URL as string;
-const envApiKey = import.meta.env.VITE_REACT_APP_TOLGEE_API_KEY as string;
-
-
-
-// Access environment variables using import.meta.env
-const tolgee = Tolgee()
-  .use(DevTools())
-  .use(FormatSimple())
-  .init({
-
-    language: "da-DK",
-    apiUrl: envApiURL,
-    apiKey: envApiKey,
-    
-  });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <TolgeeProvider tolgee={tolgee} fallback="Loading...">
-      <App />
-    </TolgeeProvider>
+    <App />
   </React.StrictMode>
 );
