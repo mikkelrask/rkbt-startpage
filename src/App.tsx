bfce4acda0180bffa68fbe19759c94025b4f5bb2
Mikkel Rask <mikkelrask@protonmail.com>
2023-08-08 19:08:59 +0200

    Fixed capitalization

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import BigImage from "./components/bigImage/index.tsx";
-import UpdatedAt from "./components/updatedAt/index.tsx";
+import BigImage from "./components/BigImage/index.tsx";
+import UpdatedAt from "./components/UpdatedAt/index.tsx";
 import HeaderContainer from "./components/HeaderContainer/index.tsx";
 import Categories from "./components/CategoryColumn/index.tsx";
 
