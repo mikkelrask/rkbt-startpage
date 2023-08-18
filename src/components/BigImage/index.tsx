7a8485a8adff1e7607958aa66780a1a9bd6ce7d1
Mikkel Rask <35899030+mikkelrask@users.noreply.github.com>
2023-08-19 00:39:55 +0200

    localstorage theme
    The last theme that was randomized, was stored in localstorage all the time, but since the app was rewritten, the default theme used on each load, omitting the theme name store in localstore. This is now fixed, keeping the option to switch by clicking the graphics

--- src/components/BigImage/index.tsx
+++ src/components/BigImage/index.tsx
@@ -1,3 +1,5 @@
+import React, { useEffect } from 'react';
+
 const themeRandomizer = () => {
   const theme = [
     "pastel",
@@ -13,25 +15,33 @@ const themeRandomizer = () => {
   return random;
 };
 
-const setTheme = () => {
-  const savedTheme = localStorage.getItem("theme");
-  if (savedTheme) {
-    document.documentElement.className = savedTheme;
-  }
-
-  const theme = themeRandomizer();
+const setTheme = (theme) => {
   document.documentElement.className = theme;
   localStorage.setItem("theme", theme);
 };
 
 const BigImage = () => {
+  const savedTheme = localStorage.getItem("theme");
+
+  useEffect(() => {
+    if (savedTheme) {
+      setTheme(savedTheme);
+    } else {
+      const randomTheme = themeRandomizer();
+      setTheme(randomTheme);
+    }
+  }, []);
+
   return (
     <>
       <div className="image-container">
         <img
           src="../../assets/Grafikforside.png"
           alt="Click Me"
-          onClick={setTheme}
+          onClick={() => {
+            const randomTheme = themeRandomizer();
+            setTheme(randomTheme);
+          }}
         />
       </div>
     </>
