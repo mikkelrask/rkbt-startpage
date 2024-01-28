fae3ccdadfd005c2f89a9285f4c111a0d2d3a770
Mikkel Rask <mikkelrask@protonmail.com>
2024-01-28 13:54:09 +0100

    style: follow system ui preferred light/dark mode.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import BigImage from "./components/BigImage/index.tsx";
 import UpdatedAt from "./components/UpdatedAt/index.tsx";
 import HeaderContainer from "./components/HeaderContainer/index.tsx";
 import Categories from "./components/CategoryColumn/index.tsx";
-
-import "./App.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import "App.css";
 
 function App() {
   return (
-    <>
+    <ThemeProvider defaultTheme="system" storageKey="admin-theme">
       <BigImage />
       <div className="right-container">
         <HeaderContainer />
@@ -16,7 +16,7 @@ function App() {
         </div>
       </div>
       <UpdatedAt />
-    </>
+    </ThemeProvider>
   );
 }
 
